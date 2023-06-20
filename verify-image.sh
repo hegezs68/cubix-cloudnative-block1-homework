1594a67b07ffce19f5d56183ba5802bd289b72a6
hegezs68 <hegezs68@hotmail.com>
2023-06-20 12:26:26 +0200

    Add verify block to ci workflow

--- verify-image.sh
+++ verify-image.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-docker run -d --name test -p 8080:8080 ghcr.io/hegezs68/cubix/1/homework/app:springboot > /dev/null
+sudo docker run -d --name test -p 8088:8088 ghcr.io/hegezs68/cubix/1/homework/app:springboot > /dev/null
 sleep 10
-curl --fail http://localhost:8080/application/test
+curl --fail http://localhost:8088/application/test
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo "Verification failed, here are the logs"
   docker logs test
 fi
-docker stop test > /dev/null
-docker rm test > /dev/null
+sudo docker stop test > /dev/null
+sudo docker rm test > /dev/null
 exit $RESULT
